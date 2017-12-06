c26b9a66484081abe1cdfa4815d9591021620f7b
Rhett Del Fierro <thinquetank@gmail.com>
2017-12-05 21:03:20 -0800

    compiling

--- src/Queries/GameQueries.hs
+++ src/Queries/GameQueries.hs
@@ -1,14 +1,13 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE OverloadedStrings #-}
 module Queries.GameQueries where
 
 --import Database.MongoDB
 --import           Data.Aeson
 --import qualified Data.ByteString.Char8 as S8
-import           Network.Wreq
-import Control.Lens
-import Data.Aeson (Value)
-import Data.Aeson.Lens (key, nth)
-import Data.ByteString.Internal
+import Network.HTTP.Simple
+import Data.ByteString.UTF8
 import System.Environment (getEnv)
 import Models.Games
 
@@ -18,16 +17,20 @@ username = getEnv "API_USERNAME"
 password :: IO String
 password = getEnv "API_PASSWORD"
 
-fullSeasonGames :: String
-fullSeasonGames = "https://api.mysportsfeeds.com/v1.1/pull/nba/2016-2017-regular/full_game_schedule.json"
+fullSeasonGames :: Request
+fullSeasonGames = "GET https://api.mysportsfeeds.com/v1.1/pull/nba/2016-2017-regular/full_game_schedule.json"
 
 getTeams :: IO ()
 getTeams = undefined
 
-getGames :: IO (Response Data.ByteString.Internal.ByteString)
+getGames :: IO (Response (FullGameSchedule (GameEntry Team)))
 getGames = do
-   let un = username
-       pw = password
-       opts = defaults & header "Authorization" .~ "Basic " ++ un ++ ":" ++ pw
-                       & header "Accept-Encoding" .~ ["gzip"]
-   getWith opts fullSeasonGames
+   un <- username
+   pw <- password
+   let authstr = fromString $ "Basic " ++ un ++ ":" ++ pw
+       request = setRequestHeaders [ ("Accept-Encoding", "gzip")
+                                   , ("Authorization", authstr)
+                                   ]
+               $ fullSeasonGames
+   response <- httpJSON request
+   return response
