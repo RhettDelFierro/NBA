6d59b14d11efaf06f18befb36525187938a61c0b
Rhett Del Fierro <thinquetank@gmail.com>
2017-12-05 22:02:30 -0800

    teams entry into mongo, working on games

--- src/Queries/GameQueries.hs
+++ src/Queries/GameQueries.hs
@@ -3,9 +3,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Queries.GameQueries where
 
---import Database.MongoDB
---import           Data.Aeson
---import qualified Data.ByteString.Char8 as S8
+import Database.MongoDB
+import Control.Monad.IO.Class
 import Network.HTTP.Simple
 import Data.ByteString.UTF8
 import System.Environment (getEnv)
@@ -23,14 +22,36 @@ fullSeasonGames = "GET https://api.mysportsfeeds.com/v1.1/pull/nba/2016-2017-reg
 getTeams :: IO ()
 getTeams = undefined
 
-getGames :: IO (Response (FullGameSchedule (GameEntry Team)))
-getGames = do
-   un <- username
-   pw <- password
-   let authstr = fromString $ "Basic " ++ un ++ ":" ++ pw
-       request = setRequestHeaders [ ("Accept-Encoding", "gzip")
-                                   , ("Authorization", authstr)
-                                   ]
-               $ fullSeasonGames
-   response <- httpJSON request
-   return response
+getGamesAPI :: IO [Team]
+getGamesAPI = do
+  un <- username
+  pw <- password
+  let authstr = fromString $ "Basic " ++ un ++ ":" ++ pw
+      request = setRequestHeaders [ ("Accept-Encoding", "gzip")
+                                  , ("Authorization", authstr)
+                                  ]
+              $ fullSeasonGames
+  response <- httpJSON request
+  return $ gameEntry (getResponseBody response)
+
+insertTeamsMongo :: IO [Team] -> IO [GameEntry] ->  IO ()
+insertTeamsMongo teams games = do
+  ts <- teams
+  ts <- games
+  pipe <- connect (host "127.0.0.1")
+  ts' <- access pipe master "NBA2016-2017" (insertTeams ts)
+  gs  <- access pipe master "NBA2016-2017" (insertTeams gs)
+  close pipe
+  print ts'
+
+
+
+insertTeams :: Control.Monad.IO.Class.MonadIO m => [Team] -> Action m [Value]
+insertTeams ts = insertMany "games" $ brk ts
+  where brk tms = map teamFields tms
+
+teamFields :: Team -> [Field]
+teamFields = (\t -> [ "tid" =: tid t
+                    , "city" =: city t
+                    , "name" =: name t
+                    , "abbreviation" =: abbreviation t])
