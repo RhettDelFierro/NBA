f22c99bab65ac1773b0233bf1ea3670942c16f39
Rhett Del Fierro <thinquetank@gmail.com>
2017-12-14 21:53:40 -0800

    almost working

--- src/Queries/BoxScoreQueries.hs
+++ src/Queries/BoxScoreQueries.hs
@@ -5,6 +5,7 @@
 module Queries.BoxScoreQueries where
 
 import Control.Concurrent.Async
+import Control.Monad.IO.Class
 import qualified Data.Bson as B
 import Data.ByteString.Base64
 import Data.ByteString.UTF8 hiding (foldl)
@@ -12,20 +13,16 @@ import Data.Foldable (foldl)
 import Data.Monoid
 import Database.MongoDB
 import Models.BoxScores
-
---import Models.Games hiding (awayTeam, homeTeam)
+import Models.Games (eid)
 import Network.HTTP.Simple
 import Network.HTTP.Client (setQueryString)
 import Queries.GameQueries (username, password)
 
---get records from DB
---use this to perform queries on the urls:
---mapConcurrently :: Traversable t => (a -> IO b) -> t a -> IO (t b)
---put records into db from queries
---pages <- mapConcurrently getURL ["url1", "url2", "url3"]
+data StatsWithGameID = StatsWithGameID String TeamStats
 
 singleBoxscoreReq :: Request
-singleBoxscoreReq = "GET https://api.mysportsfeeds.com/v1.1/pull/nba/2016-2017-regular/game_boxscore.json?"
+singleBoxscoreReq =
+  "GET https://api.mysportsfeeds.com/v1.1/pull/nba/2016-2017-regular/game_boxscore.json?"
 
 buildQuery :: ByteString -> [(ByteString, Maybe ByteString)]
 buildQuery gameid = [("gameid", Just gameid)]
@@ -61,17 +58,43 @@ buildReqStrs = do
   ids <- access pipe master "NBA2016-2017" allGameIds
   return $ map (buildQuery . fromString) ids
 
-makeReqs :: IO [TeamStats]
+makeReqs :: IO [StatsWithGameID]
 makeReqs = do
   qs <- buildReqStrs
   bs <- mapConcurrently getOneBoxScoreAPI qs
   return $ combineBoxScores bs
 
-combineBoxScores :: [GameBoxScore] -> [TeamStats]
-combineBoxScores bs = foldl (\a -> \b -> (<>) [(awayTeam b), (homeTeam b)] a) [] bs
--- makeReq :: IO ()
--- makeReq = do
---   games <- getAllGamesMongo
---   ids   <- accumulateGameIDS games
---   score <- map getOneBoxScoreAPI $ map buildQuery ids
---   return ()
+combineBoxScores :: [GameBoxScore] -> [StatsWithGameID]
+combineBoxScores bs =
+  foldl (\a -> \b -> (<>) [StatsWithGameID (eid $ game b) (awayTeam b)
+                          , StatsWithGameID (eid $ game b) (homeTeam b)] a) [] bs
+
+insertBoxScoresMongo :: [StatsWithGameID] -> IO ()
+insertBoxScoresMongo ts = do
+  pipe <- connect (host "127.0.0.1")
+  _ <- access pipe master "NBA2016-2017" (insertBoxScores ts)
+  close pipe
+
+insertBoxScores :: MonadIO m => [StatsWithGameID] -> Action m [Value]
+insertBoxScores ts = insertMany "boxscores" $ map makeBoxScoreFields ts
+
+makeBoxScoreFields :: StatsWithGameID -> [Field]
+makeBoxScoreFields (StatsWithGameID gid ts) =
+                     [ "gid" =: gid
+                     , "fg2PtAtt" =: (read $ fg2PtAtt ts :: Float)
+                     , "fg2PtMade" =: (read $ fg2PtMade ts :: Float)
+                     , "fg3PtAtt" =: (read $ fg3PtAtt ts :: Float)
+                     , "fg3PtMade" =: (read $ fg3PtMade ts :: Float)
+                     , "ftAtt" =: (read $ ftAtt ts :: Float)
+                     , "ftMade" =: (read $ ftMade ts :: Float)
+                     , "offReb" =: (read $ offReb ts :: Float)
+                     , "defReb" =: (read $ defReb ts :: Float)
+                     , "ast" =: (read $ ast ts :: Float)
+                     , "pts" =: (read $ pts ts :: Float)
+                     , "tov" =: (read $ tov ts :: Float)
+                     , "stl" =: (read $ stl ts :: Float)
+                     , "blk" =: (read $ blk ts :: Float)
+                     , "fouls" =: (read $ fouls ts :: Float)
+                     , "techFouls" =: (read $ techFouls ts :: Float)
+                     , "plusMinus" =: (read $ plusMinus ts :: Float)]
+
